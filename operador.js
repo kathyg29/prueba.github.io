c52ee246042aa16acafa9ae6c5f3e1f904c98a49
kathyg29 <129110831+kathyg29@users.noreply.github.com>
2023-04-24 15:08:52 -0500

    main

---
+++ operador.js
@@ -0,0 +1,8 @@
+//CONVERSION LARGA:
+//var celsius = prompt("valor para celsius");
+//celsius = parseFloat(celsius);
+
+//CONVERSION CORTA:
+var celsius1 = parseFloat(promp("valor de celsius"));
+var f = 9/5*celsius+32
+document.write("la conversion es: ", f);
