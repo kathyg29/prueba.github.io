5688274fb618a2aeca205f1b788c5f95cf02e2bb
kathyg29 <129110831+kathyg29@users.noreply.github.com>
2023-05-15 15:06:27 -0500

    main

--- todos los archivos/script1.js
+++ todos los archivos/script1.js
@@ -1,4 +1,4 @@
 var edad = prompt("When do you born?");
-var year = edad - 2023;
-var total = parseint(edad)
+var year = 2023 - edad;
+var total = parseint(edad);
 document.write( "Naciste en: " , total);
